982ad2922eb1d3592dc86690cee674ebc4f20b4f
MichGlz <migu0403@stud.kea.dk>
2021-02-08 23:08:47 +0100

    dynamic productlist

---
+++ product.js
@@ -0,0 +1,29 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1531";
+
+//fetch the data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+//populate the page
+function showProduct(product) {
+  console.log(product);
+
+  document.querySelectorAll(".brand").forEach((item) => {
+    item.textContent = product.brandname;
+  });
+  document.querySelectorAll(".productname").forEach((item) => {
+    item.textContent = product.productdisplayname;
+  });
+
+  document.querySelector(
+    "img.productimg"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector("img.productimg").alt = product.productdisplayname;
+  document.querySelector(".price").textContent = product.price;
+  document.querySelector(".category").textContent = product.category;
+  document.querySelector(".subcategory").textContent = product.subcategory;
+  document.querySelector(".baseColour").textContent = product.basecolour;
+  document.querySelector(".idNo").textContent = product.id;
+  document.querySelector(".brandbio").textContent = product.brandbio;
+}
---
+++ productlist.js
@@ -0,0 +1,22 @@
+const url = "http://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    handleProductList(data);
+  });
+
+function handleProductList(data) {
+  //console.log(data);
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //grab the template
+  //clone it
+  //change content
+  //grab parent
+  //append
+}
